50fc6f54810330921903515a3bd0c20abcddc86c
KrisjanisP <krisjanispetrucena@gmail.com>
2023-06-16 15:10:55 +0300

    use tabs

--- h6/main.cpp
+++ h6/main.cpp
@@ -22,59 +22,59 @@ Krišjānis Petručeņa kp22084
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    // parse command line arguments
-    if (argc != 2) {
-        cout << "Usage: " << argv[0] << " <input_file>" << endl;
-        return 1;
-    }
+	// parse command line arguments
+	if (argc != 2) {
+		cout << "Usage: " << argv[0] << " <input_file>" << endl;
+		return 1;
+	}
 
-    // atveram failu
-    ifstream input_file(argv[1]);
-    if (!input_file) {
-        cout << "Could not open input file '" << argv[1] << "'!" << endl;
-        return 1;
-    }
+	// atveram failu
+	ifstream input_file(argv[1]);
+	if (!input_file) {
+		cout << "Could not open input file '" << argv[1] << "'!" << endl;
+		return 1;
+	}
 
-    // izveidojam map struktūru, lai glabātu blakusesošo burtu pāru skaitus
-    map<pair<char,char>, int> pairs;
-    
-    // ielasam failu pa rindiņai
-    string line;
-    while (getline(input_file, line)) {
-        // pārlasam visus simbolus šajā rindā
-        for (int i = 0; i < line.length() - 1; i++) {
-            // apskatam tagadējo un nākamo simbolu
-            char current = line[i];
-            char next = line[i + 1];
+	// izveidojam map struktūru, lai glabātu blakusesošo burtu pāru skaitus
+	map<pair<char,char>, int> pairs;
+	
+	// ielasam failu pa rindiņai
+	string line;
+	while (getline(input_file, line)) {
+		// pārlasam visus simbolus šajā rindā
+		for (int i = 0; i < line.length() - 1; i++) {
+			// apskatam tagadējo un nākamo simbolu
+			char current = line[i];
+			char next = line[i + 1];
 
-            // pārvēršam abus burtus uz apakšējo reģistru
-            current = tolower(current);
-            next = tolower(next);
+			// pārvēršam abus burtus uz apakšējo reģistru
+			current = tolower(current);
+			next = tolower(next);
 
-            // ja šis vai nākamais simbols nav burts, izlaižam
-            if (!isalpha(current) || !isalpha(next)) 
-                continue;
-            
-            // palielinam skaitu
-            pairs[{current, next}]++;
-        }
-    }
+			// ja šis vai nākamais simbols nav burts, izlaižam
+			if (!isalpha(current) || !isalpha(next)) 
+				continue;
+			
+			// palielinam skaitu
+			pairs[{current, next}]++;
+		}
+	}
 
-    // izveidojam struktūru multimap, kas attiecīgajam skaitam glabā pāri
-    // es to izmantoju kā alternatīvu rezultātu kārtošanai. nezinu kāpēc
-    multimap<int, pair<char,char>, greater<int>> sorted_pairs;
-    for (auto pair : pairs) {
-        sorted_pairs.insert({pair.second, pair.first});
-    }
+	// izveidojam struktūru multimap, kas attiecīgajam skaitam glabā pāri
+	// es to izmantoju kā alternatīvu rezultātu kārtošanai. nezinu kāpēc
+	multimap<int, pair<char,char>, greater<int>> sorted_pairs;
+	for (auto pair : pairs) {
+		sorted_pairs.insert({pair.second, pair.first});
+	}
 
-    // izvadam pārus ar to attiecīgo skaitiem
-    for (auto pair : sorted_pairs) {
-        cout << pair.second.first << pair.second.second << " " << pair.first << endl;
-    }
+	// izvadam pārus ar to attiecīgo skaitiem
+	for (auto pair : sorted_pairs) {
+		cout << pair.second.first << pair.second.second << " " << pair.first << endl;
+	}
 
-    // izvadam, ka failā nav burtu pāru
-    if(sorted_pairs.size() < 1) {
-        cout<<"File doesn't have letter pairs"<<endl;
-    }
+	// izvadam, ka failā nav burtu pāru
+	if(sorted_pairs.size() < 1) {
+		cout<<"File doesn't have letter pairs"<<endl;
+	}
 
 }
